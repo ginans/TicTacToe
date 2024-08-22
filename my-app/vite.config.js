08567de9a566bbe00fbfa68ffa494eaaaf2679db
Gina Norambuena <dev.ginanorambuenas@gmail.com>
2024-08-22 07:19:34 +0000

    cree devcontainer

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tictactoe/",
+  base: "/",
   server: {
     port: 5173,
     host: true, // Cambiamos '0.0.0.0' por true
